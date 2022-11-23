928f6397e4c18faa2e0cda1316c19bdde5bcc584
Kamil Górzyński <kamilgorzynski@gmail.com>
2022-11-23 12:30:27 +0100

    continue

--- src/Exercise15.java
+++ src/Exercise15.java
@@ -1,8 +1,19 @@
 public class Exercise15 {
-    public static void main(String[]args){
+    public static void main(String[] args) {
 
 //        15. Write a Java program to swap two variables.
 
+        int a = 6;
+        int b = 10;
+        int temp;
+
+        System.out.println("Values of variables before swapping: " + a + ", " + b);
+
+        temp=a;
+        a=b;
+        b=temp;
+
+        System.out.println("Values of variables after swapping: " + a + ", " + b);
 
     }
 }
