9aead69c269b822dea5385c6f04f7c52ac05da33
Kamil Górzyński <kamilgorzynski@gmail.com>
2022-11-23 11:49:28 +0100

    Initial commit

---
+++ src/Exercise15.java
@@ -0,0 +1,8 @@
+public class Exercise15 {
+    public static void main(String[]args){
+
+//        15. Write a Java program to swap two variables.
+
+
+    }
+}
---
+++ src/Exercise9.java
@@ -0,0 +1,13 @@
+public class Exercise9 {
+    public static void main(String[] args) {
+
+//        9. Write a Java program to compute the specified expressions and print the output.
+//        Test Data:
+//        ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+//        Expected Output
+//        2.138888888888889
+
+        Double result = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        System.out.println("Result of ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)) = " + result);
+    }
+}
